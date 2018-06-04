6540810d19cea13647e71c63ff5ff072e002d29e
vamshidhar <vamshidhar@vamshidhars-mbp.t-mobile.com>
2018-06-03 22:27:46 -0500

    new project

---
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import java.util.Hashtable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Person;
+import com.example.demo.service.PersonService;
+
+
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+	
+	@Autowired
+	PersonService ps;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public Hashtable<String, Person> getAll() {
+		return ps.getAll();
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public Person getPerson(@PathVariable("id") String id) {
+		System.out.println("id:: "+id);
+		return ps.getPerson(id);
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public boolean removePerson(@PathVariable("id") String id) {
+		return ps.removePerson(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void addPerson(@RequestBody Person person) {
+		ps.addPerson(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean udpatePerson(@RequestBody Person person) {
+		return ps.updatePerson(person);
+	}
+}
---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,59 @@
+package com.example.demo.service;
+
+import java.util.Hashtable;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Person;
+
+
+@Service
+public class PersonService {
+	
+	private Hashtable<String, Person> persons = new Hashtable<>();
+	
+	public PersonService() {
+		Person p1 = new Person(1, "Vamshidhar", "Kurapati", 30);
+		Person p2 = new Person(2, "Manasa", "Kurapati", 26);
+		persons.put("1", p1);
+		persons.put("2", p2);
+	}
+	
+	public Person getPerson(String id) {
+		if(persons.containsKey(id)) {
+			return persons.get(id);
+		}
+		return null;
+	}
+	
+	public Hashtable<String, Person> getAll() {
+		return persons;
+	}
+	
+	public boolean removePerson(String id){
+		if(persons.containsKey(id)) {
+			persons.remove(id);
+			return true;
+		}
+		return false;
+	}
+	
+	public void addPerson(Person person) {
+		persons.put(String.valueOf(persons.size()+1), person);
+	}
+	
+	public boolean updatePerson(Person person) {
+		Person p = persons.get(person.getId());
+		if(p != null) {
+			p.setAge(person.getAge());
+			p.setFirstName(person.getFirstName());
+			p.setLastName(person.getLastName());
+			p.setId(person.getId());
+			persons.remove(person.getId());
+			persons.put(String.valueOf(person.getId()), person);
+			return true;
+		}
+		return false;
+	}
+
+}
