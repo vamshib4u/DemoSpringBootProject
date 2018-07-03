a5e87300cd2d55d36a41fc3d8593c4bbe69bcbbd
vamshidhar <vamshidhar@vamshidhars-mbp.t-mobile.com>
2018-07-03 00:46:30 -0500

    Removed imports

--- src/main/java/com/example/demo/controller/MyRetailerController.java
+++ src/main/java/com/example/demo/controller/MyRetailerController.java
@@ -4,7 +4,6 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
