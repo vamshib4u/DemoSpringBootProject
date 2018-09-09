301d2c6302a9c7eb81bb65640221fffbb6713b5c
Vamshidhar Raju Kurapati <vamshib4u@gmaila.com>
2018-09-09 03:24:21 -0500

    missing class check in

---
+++ src/main/java/com/example/demo/entity/StatusObject.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+public class StatusObject {
+	
+	private boolean success;
+	private String errorMessage;
+	
+	/**
+	 * @return the success
+	 */
+	public boolean isSuccess() {
+		return success;
+	}
+	/**
+	 * @param success the success to set
+	 */
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	/**
+	 * @return the errorMessage
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	/**
+	 * @param errorMessage the errorMessage to set
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	
+	
+
+}
