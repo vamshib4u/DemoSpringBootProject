de149ef113e3e44108ddffae4a2d284a93d71649
vamshib4u <39181356+vamshib4u@users.noreply.github.com>
2018-10-25 00:39:35 -0500

    Update shellScript.sh

--- src/main/resources/static/shellScript.sh
+++ src/main/resources/static/shellScript.sh
@@ -1,1 +1,1 @@
-sudo cp target/DemoSpringBootProject-0.0.1-SNAPSHOT.jar /Users/vamshidhar/dev/DemoSpringBootProject-0.0.1-SNAPSHOT.jar'
+cp target/DemoSpringBootProject-0.0.1-SNAPSHOT.jar /Users/vamshidhar/dev/DemoSpringBootProject-0.0.1-SNAPSHOT.jar'
