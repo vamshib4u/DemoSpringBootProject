89bf8473aa85310974be84c6b6af7a3874474964
vamshib4u <39181356+vamshib4u@users.noreply.github.com>
2018-10-26 00:53:55 -0500

    Update shellScript.sh

--- src/main/resources/static/shellScript.sh
+++ src/main/resources/static/shellScript.sh
@@ -1,4 +1,1 @@
 sudo cp target/DemoSpringBootProject-0.0.1-SNAPSHOT.jar /Users/vamshidhar/dev/DemoSpringBootProject-0.0.1-SNAPSHOT.jar
-cd /Users/vamshidhar/dev
-bash cf login -a https://api.local.pcfdev.io --skip-ssl-validation -u user -p pass -o pcfdev-org
-bash cf push localhost -p DemoSpringBootProject-0.0.1-SNAPSHOT.jar
