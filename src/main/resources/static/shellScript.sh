85742d358f91b99c0043b88c911bf6241fda9bc1
Vamshidhar Raju Kurapati <vamshib4u@gmaila.com>
2018-11-01 23:35:45 -0500

    updated

--- src/main/resources/static/shellScript.sh
+++ src/main/resources/static/shellScript.sh
@@ -1,4 +1,4 @@
-#sudo cp target/DemoSpringBootProject-0.0.1-SNAPSHOT.jar /Users/vamshidhar/dev/DemoSpringBootProject-0.0.1-SNAPSHOT.jar'
+#sudo cp target/DemoSpringBootProject-0.0.1-SNAPSHOT.jar /Users/vamshidhar/dev/DemoSpringBootProject-0.0.1-SNAPSHOT.jar
 cd /Users/vamshidhar/dev
 mkdir sample
 echo created sample folder
